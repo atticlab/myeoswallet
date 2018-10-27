f0288a7d9320e03163ffdd62becec41378b8be52
rengen <rengen12@gmail.com>
2018-10-27 16:06:34 +0300

    changeDir

--- docker/nodejs/entrypoint_stage.sh
+++ docker/nodejs/entrypoint_stage.sh
@@ -2,6 +2,11 @@
 
 apt update && apt-get -y install libusb-1.0-0-dev && apt-get -y install libudev-dev
 
-npm install
+rm -rf .config
+rm -rf  .node-gyp
+rm -rf node_modules
+rm -rf .npm
+
+yarn install
 
 npm run build
