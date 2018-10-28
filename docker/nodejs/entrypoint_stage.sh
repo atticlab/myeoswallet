a258529f526dc790de11ff05e6a922b53022eca0
rengen <rengen12@gmail.com>
2018-10-28 20:45:16 +0200

    fixDeploy

--- docker/nodejs/entrypoint_stage.sh
+++ docker/nodejs/entrypoint_stage.sh
@@ -2,11 +2,6 @@
 
 apt update && apt-get -y install libusb-1.0-0-dev && apt-get -y install libudev-dev
 
-rm -rf .config
-rm -rf  .node-gyp
-rm -rf node_modules
-rm -rf .npm
-
 yarn install
 
 npm run build
