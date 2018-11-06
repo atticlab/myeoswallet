efe9c874100fe4b4605d5d28d03a676940f87257
rengen1 <rengen12@gmail.com>
2018-11-06 18:10:29 +0200

    mobile

--- src/components/MobileDevice.vue
+++ src/components/MobileDevice.vue
@@ -1,14 +1,14 @@
 <template>
 <div id="main">
   <div class="row">
-    <div class="col-md-8 col-12">
+    <div class="col-md-12 col-12">
       <div class="card">
         <div class="card-header"><h4>Please follow and use our web version while mobile about to launch.
           Appreciate your choice, keeping you on the safe side</h4></div>
         <div class="card-body">
           <div class="row">
-            <div style="text-align: center">
-              <img src="@/assets/phone.png" alt="phone">
+            <div class="mx-auto">
+              <img src="/static/img/phone.png" alt="phone">
             </div>
           </div>
         </div>
@@ -29,6 +29,7 @@ export default {
     margin-top: 50px;
     text-align: center;
   }
+
   img {
     width: 20vh;
   }
